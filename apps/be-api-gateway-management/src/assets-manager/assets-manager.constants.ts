04e107296c36072e106149afce3190797b3332c2
Artem Karatai <artyom.karatai@gmail.com>
2023-06-01 19:07:56 +0200

    [be-api-gateway-management] define assets-manager controller handlers for commands and queries

---
+++ apps/be-api-gateway-management/src/assets-manager/assets-manager.constants.ts
@@ -0,0 +1,21 @@
+const ENDPOINT_ASSETS_SLUG = 'assets';
+
+/**
+ * Command slugs
+ */
+const UPLOAD_ASSET_SLUG = 'upload';
+const ARCHIVE_ASSET_SLUG = 'archive';
+
+/**
+ * Query slugs
+ */
+const FIND_MANY_ASSETS_ACTIVE_SLUG = 'find-many-active';
+const FIND_MANY_ASSETS_ARCHIVED_SLUG = 'find-many-archived';
+
+export {
+  ENDPOINT_ASSETS_SLUG,
+  UPLOAD_ASSET_SLUG,
+  ARCHIVE_ASSET_SLUG,
+  FIND_MANY_ASSETS_ACTIVE_SLUG,
+  FIND_MANY_ASSETS_ARCHIVED_SLUG,
+};
--- apps/be-api-gateway-management/src/assets-manager/assets-manager.controller.ts
+++ apps/be-api-gateway-management/src/assets-manager/assets-manager.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import {
+  ARCHIVE_ASSET_SLUG,
+  ENDPOINT_ASSETS_SLUG,
+  FIND_MANY_ASSETS_ACTIVE_SLUG,
+  FIND_MANY_ASSETS_ARCHIVED_SLUG,
+  UPLOAD_ASSET_SLUG,
+} from './assets-manager.constants';
 
-@Controller('assets-manager')
-export class AssetsManagerController {}
+@Controller(ENDPOINT_ASSETS_SLUG)
+export class AssetsManagerController {
+  @Post(UPLOAD_ASSET_SLUG)
+  upload() {
+    return;
+  }
+
+  @Post(ARCHIVE_ASSET_SLUG)
+  archive() {
+    return;
+  }
+
+  @Post(FIND_MANY_ASSETS_ACTIVE_SLUG)
+  findManyActive() {
+    return;
+  }
+
+  @Post(FIND_MANY_ASSETS_ARCHIVED_SLUG)
+  findManyArchived() {
+    return;
+  }
+}
