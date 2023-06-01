8931af326bd759a86031c8678abd3980ca5e8cf2
Artem Karatai <artyom.karatai@gmail.com>
2023-06-01 18:46:06 +0200

    [be-api-gateway-management] define pages controller handlers for commands and queries

---
+++ apps/be-api-gateway-management/src/pages/constants.ts
@@ -0,0 +1,31 @@
+const ENDPOINT_SLUG = 'pages';
+
+/**
+ * Command slugs
+ */
+const CREATE_PAGE_DRAFT_SLUG = 'create-draft';
+const ARCHIVE_PAGE_SLUG = 'archive';
+const PUBLISH_PAGE_SLUG = 'publish';
+const UNPUBLISH_PAGE_SLUG = 'unpublish';
+const SET_PAGE_ELEMENT_VALUE_SLUG = 'set-element-value';
+
+/**
+ * Query slugs
+ */
+const FIND_ONE_PAGE_DRAFT_SLUG = 'find-one-draft';
+const FIND_MANY_PAGE_DRAFTS_SLUG = 'find-many-drafts';
+const FIND_ONE_PAGE_PUBLISHED_SLUG = 'find-one-published';
+const FIND_MANY_PAGES_PUBLISHED_SLUG = 'find-many-published';
+
+export {
+  ENDPOINT_SLUG,
+  CREATE_PAGE_DRAFT_SLUG,
+  ARCHIVE_PAGE_SLUG,
+  PUBLISH_PAGE_SLUG,
+  UNPUBLISH_PAGE_SLUG,
+  SET_PAGE_ELEMENT_VALUE_SLUG,
+  FIND_ONE_PAGE_DRAFT_SLUG,
+  FIND_MANY_PAGE_DRAFTS_SLUG,
+  FIND_ONE_PAGE_PUBLISHED_SLUG,
+  FIND_MANY_PAGES_PUBLISHED_SLUG,
+};
--- apps/be-api-gateway-management/src/pages/pages.controller.ts
+++ apps/be-api-gateway-management/src/pages/pages.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import {
+  ENDPOINT_SLUG,
+  CREATE_PAGE_DRAFT_SLUG,
+  ARCHIVE_PAGE_SLUG,
+  PUBLISH_PAGE_SLUG,
+  UNPUBLISH_PAGE_SLUG,
+  SET_PAGE_ELEMENT_VALUE_SLUG,
+  FIND_ONE_PAGE_DRAFT_SLUG,
+  FIND_MANY_PAGE_DRAFTS_SLUG,
+  FIND_ONE_PAGE_PUBLISHED_SLUG,
+  FIND_MANY_PAGES_PUBLISHED_SLUG,
+} from './constants';
 
-@Controller('pages')
-export class PagesController {}
+@Controller(ENDPOINT_SLUG)
+export class PagesController {
+  @Post(CREATE_PAGE_DRAFT_SLUG)
+  createDraft() {
+    return;
+  }
+
+  @Post(ARCHIVE_PAGE_SLUG)
+  archive() {
+    return;
+  }
+
+  @Post(PUBLISH_PAGE_SLUG)
+  publish() {
+    return;
+  }
+
+  @Post(UNPUBLISH_PAGE_SLUG)
+  unpublish() {
+    return;
+  }
+
+  @Post(SET_PAGE_ELEMENT_VALUE_SLUG)
+  setElementValue() {
+    return;
+  }
+
+  @Get(FIND_ONE_PAGE_DRAFT_SLUG)
+  findOneDraft() {
+    return;
+  }
+
+  @Get(FIND_MANY_PAGE_DRAFTS_SLUG)
+  findManyDrafts() {
+    return;
+  }
+
+  @Get(FIND_ONE_PAGE_PUBLISHED_SLUG)
+  findOnePublished() {
+    return;
+  }
+
+  @Get(FIND_MANY_PAGES_PUBLISHED_SLUG)
+  findManyPublished() {
+    return;
+  }
+}
