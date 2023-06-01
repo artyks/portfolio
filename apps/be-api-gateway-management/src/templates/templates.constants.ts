58fe7d05264c348e1cfea82486a64725c5fe3c5e
Artem Karatai <artyom.karatai@gmail.com>
2023-06-01 19:00:43 +0200

    [be-api-gateway-management] define templates controller handlers for commands and queries

---
+++ apps/be-api-gateway-management/src/templates/templates.constants.ts
@@ -0,0 +1,25 @@
+const ENDPOINT_TEMPLATES_SLUG = 'templates';
+
+/**
+ * Command slugs
+ */
+const CREATE_TEMPLATE_SLUG = 'create';
+const ARCHIVE_TEMPLATE_SLUG = 'archive';
+const PUBLISH_TEMPLATE__SLUG = 'publish';
+const REPLACE_TEMPLATE_SLUG = 'replace';
+
+/**
+ * Query slugs
+ */
+const FIND_ONE_TEMPLATE_SLUG = 'find-one-template';
+const FIND_MANY_TEMPLATES_SLUG = 'find-many-templates';
+
+export {
+  ENDPOINT_TEMPLATES_SLUG,
+  CREATE_TEMPLATE_SLUG,
+  ARCHIVE_TEMPLATE_SLUG,
+  PUBLISH_TEMPLATE__SLUG,
+  REPLACE_TEMPLATE_SLUG,
+  FIND_ONE_TEMPLATE_SLUG,
+  FIND_MANY_TEMPLATES_SLUG,
+};
--- apps/be-api-gateway-management/src/templates/templates.controller.ts
+++ apps/be-api-gateway-management/src/templates/templates.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import {
+  ARCHIVE_TEMPLATE_SLUG,
+  CREATE_TEMPLATE_SLUG,
+  ENDPOINT_TEMPLATES_SLUG,
+  FIND_MANY_TEMPLATES_SLUG,
+  FIND_ONE_TEMPLATE_SLUG,
+  PUBLISH_TEMPLATE__SLUG,
+  REPLACE_TEMPLATE_SLUG,
+} from './templates.constants';
 
-@Controller('templates')
-export class TemplatesController {}
+@Controller(ENDPOINT_TEMPLATES_SLUG)
+export class TemplatesController {
+  @Post(CREATE_TEMPLATE_SLUG)
+  create() {
+    return;
+  }
+
+  @Post(ARCHIVE_TEMPLATE_SLUG)
+  archive() {
+    return;
+  }
+
+  @Post(PUBLISH_TEMPLATE__SLUG)
+  publish() {
+    return;
+  }
+
+  @Post(REPLACE_TEMPLATE_SLUG)
+  replace() {
+    return;
+  }
+
+  @Get(FIND_ONE_TEMPLATE_SLUG)
+  findOne() {
+    return;
+  }
+
+  @Get(FIND_MANY_TEMPLATES_SLUG)
+  findMany() {
+    return;
+  }
+}
