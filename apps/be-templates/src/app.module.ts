70025823d747e90037222eecabc280703b8a9c04
Artem Karatai <artyom.karatai@gmail.com>
2023-06-04 12:24:52 +0200

    [app][be-templates] import cqrs module; update command controllers to execute commands

--- apps/be-templates/src/app.module.ts
+++ apps/be-templates/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TemplatesController } from './controllers/templates.controller';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [TemplatesController],
 })
 export class AppModule {}
--- apps/be-templates/src/controllers/templates.controller.ts
+++ apps/be-templates/src/controllers/templates.controller.ts
@@ -16,27 +16,34 @@ import {
   FIND_ONE_TEMPLATE_MESSAGE,
   FIND_MANY_TEMPLATES_MESSAGE,
 } from '@be-templates/constants';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateTemplateCommand } from '../commands/implementations/create-template.command';
+import { ArchiveTemplateCommand } from '../commands/implementations/archive-template.command';
+import { PublishTemplateCommand } from '../commands/implementations/publish-template.command';
+import { ReplaceTemplateCommand } from '../commands/implementations/replace-template.command';
 
 @Controller()
 export class TemplatesController {
+  constructor(private readonly commandBus: CommandBus) {}
+
   @EventPattern(CREATE_TEMPLATE_EVENT)
-  handleTemplateCreate(@Payload() payload: CreateTemplateDto) {
-    return;
+  async handleTemplateCreate(@Payload() payload: CreateTemplateDto) {
+    return this.commandBus.execute(new CreateTemplateCommand(payload));
   }
 
   @EventPattern(ARCHIVE_TEMPLATE_EVENT)
-  handleTemplateArchive(@Payload() payload: ArchiveTemplateDto) {
-    return;
+  async handleTemplateArchive(@Payload() payload: ArchiveTemplateDto) {
+    return this.commandBus.execute(new ArchiveTemplateCommand(payload));
   }
 
   @EventPattern(PUBLISH_TEMPLATE_EVENT)
-  handleTemplatePublish(@Payload() payload: PublishTemplateDto) {
-    return;
+  async handleTemplatePublish(@Payload() payload: PublishTemplateDto) {
+    return this.commandBus.execute(new PublishTemplateCommand(payload));
   }
 
   @EventPattern(REPLACE_TEMPLATE_EVENT)
-  handleTemplateReplace(@Payload() payload: ReplaceTemplateDto) {
-    return;
+  async handleTemplateReplace(@Payload() payload: ReplaceTemplateDto) {
+    return this.commandBus.execute(new ReplaceTemplateCommand(payload));
   }
 
   @MessagePattern(FIND_ONE_TEMPLATE_MESSAGE)
