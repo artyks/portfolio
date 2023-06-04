2d1509c58d76f628a73174276bd933baf6d92c27
Artem Karatai <artyom.karatai@gmail.com>
2023-06-04 12:28:55 +0200

    [app][be-assets-manager] import cqrs module; update command controllers to execute commands

--- apps/be-assets-manager/src/assets/assets.module.ts
+++ apps/be-assets-manager/src/assets/assets.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AssetsController } from './controllers/assets/assets.controller';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [AssetsController],
 })
 export class AssetsModule {}
--- apps/be-assets-manager/src/assets/controllers/assets/assets.controller.ts
+++ apps/be-assets-manager/src/assets/controllers/assets/assets.controller.ts
@@ -1,18 +1,23 @@
 import { UPLOAD_ASSET_EVENT, ARCHIVE_ASSET_EVENT, FIND_MANY_ASSETS_MESSAGE } from '@be-assets-manager/constants';
 import { ArchiveAssetDto, FindManyAssetsDto, UploadAssetDto } from '@be-assets-manager/dtos';
 import { Controller } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
 import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import { UploadAssetCommand } from '../../commands/implementations/upload-asset.command';
+import { ArchiveAssetCommand } from '../../commands/implementations/archive-asset.command';
 
 @Controller()
 class AssetsController {
+  constructor(private readonly commandBus: CommandBus) {}
+
   @EventPattern(UPLOAD_ASSET_EVENT)
-  handleAssetUpload(@Payload() payload: UploadAssetDto) {
-    return;
+  async handleAssetUpload(@Payload() payload: UploadAssetDto) {
+    return this.commandBus.execute(new UploadAssetCommand(payload));
   }
 
   @EventPattern(ARCHIVE_ASSET_EVENT)
-  handleAssetArchive(@Payload() payload: ArchiveAssetDto) {
-    return;
+  async handleAssetArchive(@Payload() payload: ArchiveAssetDto) {
+    return this.commandBus.execute(new ArchiveAssetCommand(payload));
   }
 
   @MessagePattern(FIND_MANY_ASSETS_MESSAGE)
